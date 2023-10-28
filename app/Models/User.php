28bb6b090c352e4ced5f5dd0690537ed07e27828
root <nishi3da@gmail.com>
2023-10-28 17:48:08 +0900

    認証用にDBのUsesテーブルにカラム追加

--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,10 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'role_id',
+        'reading_name',
+        'affiliation_name',
+        'affiliation_reading_name',
     ];
 
     /**
---
+++ database/migrations/2023_10_28_173318_add_users_table_2columns.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->foreignId('role_id')->constrained()->after('remember_token');
+            $table->string('reading_name')->after('name');
+            $table->string('affiliation_name')->after('reading_name');
+            $table->string('affiliation_reading_name')->after('affiliation_name');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropForeign(['role_id']);
+            $table->dropColumn(['role_id', 'reading_name', 'affiliation_name', 'affiliation_reading_name']);
+        });
+    }
+};
