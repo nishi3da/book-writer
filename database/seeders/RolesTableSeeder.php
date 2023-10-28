8eea98cfd55e044c4b9e15fa417241d45288201d
root <nishi3da@gmail.com>
2023-10-28 17:32:15 +0900

    RoleのDB追加、初期データの設定

---
+++ database/seeders/RolesTableSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+use App\Models\Role;
+
+class RolesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        Role::create([
+            'name' => '管理者',
+            'level' => 0
+        ]);
+
+        Role::create([
+            'name' => '管理者',
+            'level' => 100
+        ]);
+
+        Role::create([
+            'name' => '管理者',
+            'level' => 200
+        ]);
+
+    }
+}
