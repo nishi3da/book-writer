c5fec3caab61a1134e0f44c49f165afd969ebfad
root <nishi3da@gmail.com>
2023-10-29 15:23:34 +0900

    ゲートに各権限の設定を追加

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,9 @@
 
 namespace App\Providers;
 
-// use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +22,21 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->registerPolicies();
+
+        // 「管理者」以上に適用
+        Gate::define('admin_role', function ($user) {
+            return ($user->role()->level < 100);
+        });
+
+        // 「編集者」以上に適用
+        Gate::define('editor_role', function ($user) {
+            return (100 <= $user->role()->level && $user->role()->level < 200);
+        });
+
+        // 「執筆者」以上に適用
+        Gate::define('author_role', function ($user) {
+            return ($user->role()->level >= 200);
+        });
     }
 }
