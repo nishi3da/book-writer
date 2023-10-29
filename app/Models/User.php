d6c88f63ca0fd7aa2b483681f93fa38df524d7b4
root <nishi3da@gmail.com>
2023-10-29 15:22:33 +0900

    users / rolesのリレーション設定

--- app/Models/User.php
+++ app/Models/User.php
@@ -46,4 +46,8 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function role() {
+        return $this->belongsTo(Role::class);
+    }
 }
