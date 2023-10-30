d296cbff6e1e4ccb9f7f4510fb12372e2a147436
root <nishi3da@gmail.com>
2023-10-31 00:00:33 +0900

    DatabaseSeederの登録クラス名のタイポ修正

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -21,6 +21,6 @@ class DatabaseSeeder extends Seeder
 
         $this->call(RolesTableSeeder::class);
         $this->call(UserTableSeeder::class);
-        $this->call(BookTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
     }
 }
