a015bfcaf8e114b48df112447b0ef8d7dcdc5125
root <nishi3da@gmail.com>
2023-11-01 16:54:32 +0900

    React側のラベルの型定義とラベルを登録

---
+++ resources/ts/app.d.ts
@@ -0,0 +1,13 @@
+declare type LableType = {
+    BookGrid: {
+        Header: {
+            ID: string;
+            Title: string;
+            SubTitle: string;
+            Edit: string;
+            Delete: string;
+        }
+        AddBook: string;
+    }
+
+}
---
+++ resources/ts/labels.ts
@@ -0,0 +1,12 @@
+export const L: LableType = {
+    BookGrid: {
+        Header: {
+            ID: "ID",
+            Title: "タイトル",
+            SubTitle: "サブタイトル",
+            Edit: "編集",
+            Delete: "削除",
+        },
+        AddBook: "新規書籍",
+    },
+};
