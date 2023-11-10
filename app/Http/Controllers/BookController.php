7e35579dea6db4e0b646455b04cf507d9d90ecf3
root <nishi3da@gmail.com>
2023-11-11 01:33:59 +0900

    updateアクションの実装

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -76,7 +76,27 @@ class BookController extends Controller
      */
     public function update(Request $request, string $id)
     {
-        //
+        $bookData = $request->input("bookData");
+        $title = $bookData["title"];
+        $subTitle = $bookData["sub_title"];
+        $numberOfArticles = intval($bookData["number_of_articles"]);
+        $numberOfSections = intval($bookData["number_of_sections"]);
+        $editorIds = explode(",", $bookData["editors"]);
+        $authorIds = explode(",", $bookData["authors"]);
+
+        $book = Book::find($id);
+        $book->title = $title;
+        $book->sub_title = $subTitle;
+        $book->number_of_articles = $numberOfArticles;
+        $book->number_of_sections = $numberOfSections;
+
+        $book->save();
+
+        $editors = User::whereIn('id', $editorIds)->get();
+        $book->users()->sync($editors, ['role' => 'editor']);
+
+        $authors = User::whereIn('id', $authorIds)->get();
+        $book->users()->sync($authors, ['role' => 'author']);
     }
 
     /**
