9179db6a6bd4a681db9ba717ef6a8e18eb56a22c
root <nishi3da@gmail.com>
2023-11-27 16:49:16 +0900

    ArticleControllerの新規作成

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Log;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function articles_list(string $bookId) {
+        Log::debug('--- articles_list ---');
+        $articles = Article::where('book_id', '=', $bookId)->orderBy('number', 'asc')->get();
+        return response()->json($articles);
+    }
+}
