20c836ccbe30e2fa91d2e95087dea2cf2f882b56
root <nishi3da@gmail.com>
2023-11-28 16:51:02 +0900

    バグ修正 use宣言忘れの修正

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -6,6 +6,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Log;
 use App\Models\Article;
+use App\Models\ArticleType;
 
 class ArticleController extends Controller
 {
