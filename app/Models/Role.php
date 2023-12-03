4b61d787bad4508d0b8eeee8ade3bb6c37710cc3
root <nishi3da@gmail.com>
2023-12-03 16:36:01 +0900

    admins, editors, authors, operators DBから取得し、Viewに渡す部分を修正

--- app/Models/Role.php
+++ app/Models/Role.php
@@ -7,12 +7,30 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+use App\Models\User;
+
 final class Role extends Model
 {
     use HasFactory;
 
     public function users() {
         return $this->hasMany(User::class);
+    }
+
+    public function admins() {
+        return $this->hasMany(User::class)->whereBetween('level', [0,99])->withTimestamps();
+    }
+
+    public function editors() {
+        return $this->hasMany(User::class)->whereBetween('level', [100,199])->withTimestamps();
+    }
+
+    public function authors() {
+        return $this->hasMany(User::class)->whereBetween('level', [200,299])->withTimestamps();
+    }
+
+    public function operators() {
+        return $this->hasMany(User::class)->whereBetween('level', [300,399])->withTimestamps();
     }
 
 }
